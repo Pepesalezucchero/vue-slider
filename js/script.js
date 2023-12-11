68af41ef285282617d2bc0b86db647c6a0135fb2
Alessio Rugi <alessiorugi95@gmail.com>
2023-12-11 14:54:45 +0100

    cambi in HTML per visualizzazione attraverso JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//Partendo dallo starter pack dello zip che vi passo, rifare lo slider ma questa volta usando Vue.
+
+
+/*
 const slides = [
 {
     image: 'img/01.webp',
@@ -23,3 +27,41 @@ const slides = [
 ];
 
 console.log(slides);
+*/
+
+//creo la base con Vue e ci aggiungo tutto il necessario
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            activeImage: 0,
+            activeTitle: 0,
+            activeText: 0,
+            games: {
+                images: [
+                    'img/01.webp',
+                    'img/02.webp',
+                    'img/03.webp',
+                    'img/04.webp',
+                    'img/05.webp'
+                ],
+                titles: [
+                    'Marvel\'s Spiderman Miles Morale',
+                    'Ratchet & Clank: Rift Apart',
+                    'Fortnite',
+                    'Stray',
+                    "Marvel's Avengers"
+                ],
+                texts: [
+                    'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.'
+                ]
+            },
+        }
+    },
+    
+}).mount("#app");
